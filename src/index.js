0f4b7657940a1b94b5fca74c54fc40231d3d0581
github username <github email address>
2019-10-29 16:20:55 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form')
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+
+  const toDoTask = document.querySelector('input[placeholder = "description"]').value
+
+
+  const newTask = document.createElement('li')
+  const button = document.createElement('button')
+  button.addEventListener('click', function(e){
+    e.target.parentNode.remove();
+  })
+
+
+  button.innerHTML = 'x'
+  button.id = 'delete'
+
+
+  newTask.innerText = `${toDoTask}`  
+ 
+
+  document.querySelector('#tasks').appendChild(newTask)
+  newTask.appendChild(button)
+
+})})
+ 
